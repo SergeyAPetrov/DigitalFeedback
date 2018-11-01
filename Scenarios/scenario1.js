14f14ddcee6691ff033da767f782c8c050a6188b
Sergey Petrov <sergeyp@confirmit.com>
2018-11-01 14:18:47 +0300

    test

--- Scenarios/scenario1.js
+++ Scenarios/scenario1.js
@@ -1,1 +1,20 @@
-api().show().testtesttest2();
+//gigigigi
+
+var outdated = Date.now() > new Date('2019-06-06 9:00'); // 9am on June 6th
+var declined = localStorage.getItem('last_invitation_status') === 'declined';
+
+if (outdated || declined)
+ return;
+
+var ctx =
+ api()
+     .invite('invite2')
+     .container('container2')
+     .survey('p8844038')
+     .show();
+
+ctx.events.declineInvite.on(
+ function() {
+   console.log('decline event');
+   localStorage.setItem('last_invitation_status', 'declined');
+ });
